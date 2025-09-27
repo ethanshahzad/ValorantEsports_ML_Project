a86c48c7de8a7b3efdb591cd403bb190528f420f
Ethan Shahzad <68079700+ethanshahzad@users.noreply.github.com>
2025-09-27 00:17:26 -0400

    Added recent 5 games stats
    Recent 5 combined with Recent 10 helped create capturing of teams overall performance and recent form helping with volality. Also took the difference between team A variables and team B variables to streamline the features

--- models/vlrModel.py
+++ models/vlrModel.py
@@ -20,7 +20,10 @@ df = df[(df["teamA_matches_used"] >= 10) & (df["teamB_matches_used"] >= 10)]
 print("After filtering:", df.shape)
 
 # 3. Create features and labels
-X = df.drop(columns=["winner", "link", "date"])
+X = df.drop(columns=["winner", "link", "date", "teamA_matches_used", "teamB_matches_used"])
+# y = df["winner"]
+
+X= X[[c for c in X.columns if c.startswith("diff_")]]
 y = df["winner"]
 
 # Split 80% training / 20% testing
@@ -56,31 +59,24 @@ feat_importance = pd.DataFrame({
 
 print("\nFeature importances:\n", feat_importance.to_string(index=False))
 
-X_no_ws = X_train.drop(columns=["teamA_winstreak", "teamB_winstreak"])
 
-model_no_ws = RandomForestClassifier(n_estimators=100, random_state=1234)
-model_no_ws.fit(X_no_ws, y_train)
-acc_no_ws = accuracy_score(y_test, model_no_ws.predict(X_test.drop(columns=["teamA_winstreak", "teamB_winstreak"])))
-
-print("Accuracy with winstreak:", accuracy)
-print("Accuracy without winstreak:", acc_no_ws)
 
 # ------------------------------------------------------------------------------------------------------------------------
 # Testing GridCV Model:
 
-param_grid = {
-    'n_estimators': [100, 200, 300],
-    'max_depth': [None, 10, 20],
-    'min_samples_split': [2, 5, 10]
-}
+# param_grid = {
+#     'n_estimators': [100, 200, 300],
+#     'max_depth': [None, 10, 20],
+#     'min_samples_split': [2, 5, 10]
+# }
 
-grid = GridSearchCV(
-    RandomForestClassifier(random_state=1234),
-    param_grid,
-    cv=5,
-    scoring='accuracy'
-)
-grid.fit(X_train, y_train)
+# grid = GridSearchCV(
+#     RandomForestClassifier(random_state=1234),
+#     param_grid,
+#     cv=5,
+#     scoring='accuracy'
+# )
+# grid.fit(X_train, y_train)
 
-print("Best params:", grid.best_params_)
-print("Best CV score:", grid.best_score_)
+# print("Best params:", grid.best_params_)
+# print("Best CV score:", grid.best_score_)
